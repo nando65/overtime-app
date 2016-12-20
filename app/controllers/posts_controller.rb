c3900e890a127c7745a5825ba75ac9bce8adbecc
Fernando <nando65@hotmail.com>
2016-12-20 18:29:14 -0400

    created post forms and refactor

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,19 @@
 class PostsController < ApplicationController
+  before_action :set_post, only:[:show]
   def index
+  end
+
+  def new
+    @post=Post.new
+  end
+  def create
+    @post=Post.new(params.require(:post).permit(:date,:rationale))
+    @post.save
+    redirect_to @post
+  end
+  def show
+  end
+  def set_post
+    @post = Post.find(params[:id])
   end
 end
