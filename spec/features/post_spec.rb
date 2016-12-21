06a5c887563e2cfa044cb55eef493f3d88d1eff6
Fernando <nando65@hotmail.com>
2016-12-21 01:46:17 -0400

    rspec for table of posts

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -1,33 +1,42 @@
 require 'rails_helper'
 
 describe 'navigate' do
+
+  before do
+    @user=User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name:"Jon", last_name:"Snow")
+    login_as(@user, :scope => :user)
+    visit posts_path
+  end
   describe 'index' do
     it 'can be reached successfully' do
-      visit posts_path
       expect(page.status_code).to eq(200)
     end
 
     it 'has a title of Posts' do
-      visit posts_path
       expect(page).to have_content(/Posts/)
+    end
+
+    it 'has a list of posts' do
+      post1=Post.create(date: Date.today, rationale:"Post1", user_id: @user.id)
+      post2=Post.create(date: Date.today, rationale:"Post2", user_id: @user.id)
+      visit posts_path
+      expect(page).to have_content(/Post1|Post2/)
     end
   end
 
 describe "creation" do
 
-  before do
-    user=User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name:"Jon", last_name:"Snow")
-    login_as(user, :scope => :user)
-    visit new_post_path
-  end
+before do
+      visit new_post_path
+    end
+
 
   it 'has a new form that can be reached' do
-    visit new_post_path
+
     expect(page.status_code).to eq(200)
   end
 
   it "can be created from new form page" do
-    visit new_post_path
 
     fill_in 'post[date]', with:  Date.today
     fill_in 'post[rationale]', with: "Some rationale"
@@ -36,12 +45,12 @@ describe "creation" do
     expect(page).to have_content("Some rationale")
   end
 
-  it 'will have a user associated' do
-    fill_in 'post[date]', with:  Date.today
-    fill_in 'post[rationale]', with: "User Association"
-    click_on "Save"
+  it 'will have a user associated it' do
+      fill_in 'post[date]', with: Date.today
+      fill_in 'post[rationale]', with: "User Association"
+      click_on "Save"
 
-    expect(User.last.posts.last.rationale).to eq("User Association")
-  end
+      expect(User.last.posts.last.rationale).to eq("User Association")
+    end
   end
 end
